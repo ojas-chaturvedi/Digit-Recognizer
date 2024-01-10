d256a76241c43c585843277c505afbd1c4b2d99b
Saianshul <vishnubhaktula@gmail.com>
2024-01-09 22:11:15 -0700

    Fix bugs. Accuracy is 0.8141.

--- model/digit_recognizer.py
+++ model/digit_recognizer.py
@@ -1,21 +1,6 @@
 import numpy as np
 from keras.datasets import mnist
 
-# Bayes theorem: P(y|x) = P(x|y) * P(y) / P(x)
-    # P(y|x) is the posterior
-    # P(y) is the prior
-        # Frequency of each class
-    # P(x|y) is the likelihood
-        # = P(x_1|y) * P(x_2|y) * ... * P(x_n|y)
-            # P(x_n|y) is a feature
-# Step 1: Load the data
-# Step 2: Preprocess the data using reshape
-# fit() idk why this is called fit, just using convention
-    # Step 3: Find prior of each class (0-9) by looping through np.unique(y) and store in an array
-    # Step 4: Find mean and variance for each feature and store in an array
-# predict() - finding the class with the highest posterior (calculate y)
-    # y = argmax(P(y|x)) = argmax(P(x|y) * P(y) / P(x)) = argmax(P(x|y) * P(y)) = argmax(log(P(x_1|y) * P(x_2|y) * ... * P(x_n|y) * P(y)))
-
 class NaiveBayes:
     def __init__(self):
         self.means = []
@@ -27,21 +12,25 @@ class NaiveBayes:
         self.classes = np.unique(y)
 
         for i in self.classes:
-            self.priors.append(np.sum(y == i) / len(y))
+            self.priors.append(np.mean(y == i))
             x_n = x[y == i]
-            self.means.append(np.mean(x_n, axis = 1))
-            self.variances.append(np.var(x_n, axis = 1))
+            self.means.append(np.mean(x_n, axis = 0))
+            self.variances.append(np.var(x_n, axis = 0) + 0.01)
 
     def predict(self, x):
         for i in self.classes:
             log_prior = np.log(self.priors[i])
-            likelihood = np.sum(np.log(1 / np.sqrt(2 * np.pi * self.variances[i])) - ((x - self.means[i]) ** 2) / (2 * self.variances[i]))
+            likelihood = np.sum(np.log(self.gaussian(x, self.means[i], self.variances[i])), axis = 1)
             posterior = likelihood + log_prior
             self.posteriors.append(posterior)
-            y = np.argmax(self.posteriors)
-        
-        return y
 
+        return np.argmax(self.posteriors, axis = 0)
+
+    def gaussian(self, x, mean, variance):
+        numerator = np.exp(-((x - mean) ** 2) / (2 * variance))
+        denominator = np.sqrt(2 * np.pi * variance)
+        return numerator / denominator
+        
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(x_train.shape[0], -1) / 255.0
 x_test = x_test.reshape(x_test.shape[0], -1) / 255.0
@@ -50,4 +39,4 @@ model = NaiveBayes()
 model.fit(x_train, y_train)
 y_predicted = model.predict(x_test)
 accuracy = np.mean(y_predicted == y_test)
-print(accuracy)
+print("Accuracy: ", accuracy)
