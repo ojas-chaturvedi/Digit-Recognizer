6adf3f0f4214797d1f6de8ce0770dce38adff74c
Zaheen Jamil <zaheen.jamil@gmail.com>
2024-01-17 19:06:37 -0700

    HOG feature, 92% accuracy

--- model/digit_recognizer.py
+++ model/digit_recognizer.py
@@ -14,18 +14,29 @@ class NaiveBayes:
 
     def fit(self, x, y):
         self.classes = np.unique(y)
+        hog = cv2.HOGDescriptor((28, 28), (14, 14), (7, 7), (7, 7), 9)
 
         for i in self.classes:
             self.priors.append(np.mean(y == i))
-            x_n = x[y == i]
-            self.means.append(np.mean(x_n, axis = 0))
-            self.variances.append(np.var(x_n, axis = 0) + 0.01559)
+
+            x_i = x[y == i]
+            x_hog = np.zeros((len(x_i), 324))
+            for j in range(len(x_i)):
+                x_hog[j] = np.squeeze(hog.compute(x_i[j].reshape(28, 28)))
+
+            self.means.append(np.mean(x_hog, axis = 0))
+            self.variances.append(np.var(x_hog, axis = 0) + 0.01559)
 
     def predict(self, x):
         self.posteriors = []
+        hog = cv2.HOGDescriptor((28, 28), (14, 14), (7, 7), (7, 7), 9)
+        x_hog = np.zeros((len(x), 324))
         for i in self.classes:
+            for j in range(len(x)):
+                x_hog[j] = np.squeeze(hog.compute(x[j].reshape(28, 28)))
+
             log_prior = np.log(self.priors[i])
-            likelihood = np.sum(np.log(self.gaussian(x, self.means[i], self.variances[i])), axis = 1)
+            likelihood = np.sum(np.log(self.gaussian(x_hog, self.means[i], self.variances[i])), axis = 1)
             posterior = likelihood + log_prior
             self.posteriors.append(posterior)
         self.posteriors = np.array(self.posteriors)
@@ -44,8 +55,8 @@ class ProcessImage:
         self.path = 'random.PNG'
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(x_train.shape[0], -1) / 255.0
-x_test = x_test.reshape(x_test.shape[0], -1) / 255.0
+x_train = x_train.reshape(x_train.shape[0], -1)
+x_test = x_test.reshape(x_test.shape[0], -1)
 
 model = NaiveBayes()
 model.fit(x_train, y_train)
