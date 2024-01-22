5a0f3c4b5041f6066bb21965200bca489d6fff88
Ojas Chaturvedi <oj.chaturvedi.2024@gmail.com>
2024-01-22 12:28:07 -0700

    Fixed indentation

--- website/main.py
+++ website/main.py
@@ -136,40 +136,40 @@ html(
     """
 <html>
 <head>
-   <script src = "https://cdnjs.cloudflare.com/ajax/libs/tsparticles/1.18.11/tsparticles.min.js"> </script>
-   <style>
-      #particles {
-         position: fixed;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          width: 100%;
-          height: 100%;
-          z-index: -1;
-          overflow: hidden;
-         background-color: teal;
-      }
-   </style>
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/tsparticles/1.18.11/tsparticles.min.js"> </script>
+    <style>
+        #particles {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            width: 100%;
+            height: 100%;
+            z-index: -1;
+            overflow: hidden;
+            background-color: teal;
+        }
+    </style>
 </head>
 <body>
-   <div id = "particles">
-   </div>
-   <script>
-      tsParticles.load("particles", {
-         particles: {
-            number: {
-               value: 1000
-            },
-            move: {
-               enable: true
-            },
-            color: {
-               value: "#272701"
-            },
-         }
-      });
-   </script>
+    <div id = "particles">
+    </div>
+    <script>
+        tsParticles.load("particles", {
+            particles: {
+                number: {
+                    value: 1000
+                },
+                move: {
+                    enable: true
+                },
+                color: {
+                    value: "#272701"
+                },
+            }
+        });
+    </script>
 </body>
 </html>
 """,
