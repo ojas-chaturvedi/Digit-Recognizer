1893ff5d8eae14711ca3963e32f1b01e62c79d2b
Ojas Chaturvedi <oj.chaturvedi.2024@gmail.com>
2024-01-24 22:50:26 -0700

    Removed matplotlib -- testing

--- website/main.py
+++ website/main.py
@@ -4,7 +4,7 @@ import subprocess
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'streamlit'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'opencv-python'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'matplotlib'])
+# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'matplotlib'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'seaborn'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'scipy'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'keras'])
