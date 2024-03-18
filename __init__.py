028500f9b9f9df442e81656450cf4c5af399de7c
Ojas Chaturvedi <oj.chaturvedi.2024@gmail.com>
2024-03-18 00:55:38 -0700

    Initialize directory as a Python Package

---
+++ __init__.py
@@ -0,0 +1,17 @@
+#!/opt/homebrew/bin/python3
+"""
+Name: __init__.py
+Purpose: Initializes directory as a Python Package
+"""
+
+__author__ = "Ojas Chaturvedi"
+__github__ = "ojas-chaturvedi"
+__license__ = "MIT"
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
