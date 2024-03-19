c4a81ef0e81d7403a90b2ef5e648ae377328355c
Ojas Chaturvedi <oj.chaturvedi.2024@gmail.com>
2024-03-18 21:23:12 -0700

    Create website.py

---
+++ model/website.py
@@ -0,0 +1,10 @@
+#!/opt/homebrew/bin/python3
+"""
+Name: website.py
+Purpose: To showcase the Gaussian Naïve Bayes classifier to recognize handwritten digits
+"""
+
+__author__ = "Ojas Chaturvedi"
+__github__ = "github.com/ojas-chaturvedi"
+__license__ = "MIT"
+
